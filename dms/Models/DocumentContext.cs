ba6bb8e0523d971c0f661fb69b150f647af8780b
Cicek <ahmetcicek085@gmail.com>
2025-09-15 11:30:35 +0200

    add explanatory comments to code for better readability

--- dms/Models/DocumentContext.cs
+++ dms/Models/DocumentContext.cs
@@ -4,6 +4,11 @@ namespace dms.Models;
 
 public class DocumentContext : DbContext
 {
+    // ctor receives configuration options (eg connection string) from di container
     public DocumentContext(DbContextOptions<DocumentContext> options) : base(options) { }
+
+    // Represents the 'Documents' table in the database
+    // Enables CRUD operations on Document entities without writing raw SQL
+    // EF Core translates operations on this DbSet into SQL commands
     public DbSet<Document> Documents => Set<Document>();
 }
--- dms/Program.cs
+++ dms/Program.cs
@@ -2,38 +2,39 @@ using dms.Models;
 using dms.Dal;
 using Microsoft.EntityFrameworkCore;
 
+// host loading configurations, di container, logging
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// load documentctx into di container & configure ef core to use postgres
 builder.Services.AddDbContext<DocumentContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
 
-
+// Add services to the container
 builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Build the application host with all registered services and configuration
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope()) // create scope to get dbcontext to migrate
 {
     var ctx = scope.ServiceProvider.GetRequiredService<DocumentContext>();
-    ctx.Database.Migrate();   // erstellt/aktualisiert Tabellen
+    ctx.Database.Migrate();   // creates/updates tables
 }
 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi(); // create static json file /openapi/v1.json
+    app.UseSwagger(); // http://localhost:5032/swagger/v1/swagger.json creates dynamic json file
+    app.UseSwaggerUI(); // takes dynamic json file and creates nice ui
 }
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+app.UseHttpsRedirection(); // security, redirect HTTP to HTTPS
+app.UseAuthorization(); // entry
+app.MapControllers(); // api endpoints
+app.Run(); // starts the app
