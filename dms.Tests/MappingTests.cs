6fab96b864438f69e7d67a35931400b4a4a4eadc
Süleyman Tegmen <stegmen@iastate.edu>
2025-09-17 01:07:27 +0200

    Added auto mapping, a test and the Api layer

---
+++ dms.Tests/MappingTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using dms.Bl.Mapping;
+using dms.Api.Mapping;
+
+public class MappingTests
+{
+	[Fact]
+	public void MapperConfiguration_IsValid()
+	{
+        // Arrange
+        var services = new ServiceCollection();
+        services.AddLogging();
+
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.AddProfile<DocumentProfile>();
+            cfg.AddProfile<ApiDocumentProfile>();
+        });
+
+        var provider = services.BuildServiceProvider();
+
+        // Act improve test later!!!!!!! CHECK FOR CORRECT DATA
+        var mapper = provider.GetRequiredService<IMapper>();
+
+        // Assert
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+    }
+}
---
+++ dms/Api/Dtos/DocumentDto.cs
@@ -0,0 +1,9 @@
+namespace dms.Api.Dtos;
+
+public class DocumentDto
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string FilePath { get; set; } = default!;
+    public DateTime UploadedAt { get; set; }
+}
