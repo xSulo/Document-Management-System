83b8b49835ba2f8f15ecfc3a10db46b0b87475b2
Cicek <ahmetcicek085@gmail.com>
2025-10-07 20:31:29 +0200

    chore(logging): add service/repo logs

--- dms/Bl/Services/DocumentService.cs
+++ dms/Bl/Services/DocumentService.cs
@@ -4,6 +4,7 @@ using dms.Bl.Entities;
 using dms.Bl.Interfaces;
 using dms.Dal.Entities;
 using dms.Dal.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace dms.Bl.Services;
 
@@ -12,12 +13,14 @@ public class DocumentService : IDocumentService
     private readonly IDocumentRepository _repo;
     private readonly IValidator<BlDocument> _validator;
     private readonly IMapper _mapper;
+    private readonly ILogger<DocumentService> _log;
 
-    public DocumentService(IDocumentRepository repo, IValidator<BlDocument> validator, IMapper mapper)
+    public DocumentService(IDocumentRepository repo, IValidator<BlDocument> validator, IMapper mapper, ILogger<DocumentService> log)
     {
         _repo = repo;
         _validator = validator;
         _mapper = mapper;
+        _log = log;
     }
 
     public async Task<IReadOnlyList<BlDocument>> GetAllAsync() =>
@@ -31,11 +34,13 @@ public class DocumentService : IDocumentService
 
     public async Task<BlDocument> AddAsync(BlDocument doc)
     {
+        _log.LogInformation("Create document requested: Title={Title}", doc.Title);
         var res = await _validator.ValidateAsync(doc, o => o.IncludeRuleSets("Create"));
         if (!res.IsValid) throw new ValidationException(res.Errors);
 
         var dal = _mapper.Map<Document>(doc);
         var created = await _repo.AddAsync(dal);
+        _log.LogInformation("Document persisted: Id={Id}", created.Id);
         return _mapper.Map<BlDocument>(created);
     }
 
@@ -45,8 +50,13 @@ public class DocumentService : IDocumentService
         if (!res.IsValid) throw new ValidationException(res.Errors);
 
         var dal = _mapper.Map<Document>(doc);
+        _log.LogInformation("Update requested: Id={Id}", doc.Id);
         return await _repo.UpdateAsync(dal);
     }
 
-    public Task<bool> DeleteAsync(long id) => _repo.DeleteAsync(id);
+    public Task<bool> DeleteAsync(long id)
+    {
+        _log.LogInformation("Delete requested: Id={Id}", id);
+        return _repo.DeleteAsync(id);
+    }
 }
